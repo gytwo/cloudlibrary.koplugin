2da8391e1c740aa3dec558c503fdb9b333cbd39c
gytwo <5345778+gytwo@user.noreply.gitee.com>
2026-04-10 06:46:16 +0000

    删除文件 _meta.lua

--- _meta.lua
+++
@@ -1,8 +0,0 @@
-local _ = require("gettext")
-return {
-    name = "CloudLibrary",
-    fullname = _("云端书库"),
-    plugin_id = _("cloud_library_plugin"),
-    description = _([[在设备间同步书籍及元数据（书籍标注、阅读进度等）。]]),
-    version = "v1.0"
-}
