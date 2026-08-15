9a030c824db967d683ec10f1483433ce4547d915
gytwo <15279172411@163.com>
2026-08-15 20:29:58 +0800

    Update version to v1.4.4 in _meta.lua

--- _meta.lua
+++ _meta.lua
@@ -1,8 +1,7 @@
 local _ = require("gettext")
 return {
-    name = "CloudLibrary",
     fullname = _("Cloud Library"),
     plugin_id = _("cloud_library_plugin"),
     description = _([[Sync books and metadata (annotations, reading progress, etc.) between devices.]]),
-    version = "v1.4.3"
+    version = "v1.4.4"
 }
